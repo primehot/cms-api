64caa409972556ff0166ed88258a1530a7c38a66
oleht <oleh.tsyupa@gmail.com>
2020-03-16 22:29:46 +0100

    Initial commit

---
+++ src/test/java/com/binance/cms/api/CmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binance.cms.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
